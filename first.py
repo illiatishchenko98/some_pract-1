ac90cc3b5c1a3717cab1cf88dc4f3890cd8fe011
Illia Tishchenko <102582375+illiatishchenko98@users.noreply.github.com>
2023-03-22 15:39:02 -0500

    Add files via upload

---
+++ first.py
@@ -0,0 +1,26 @@
+# age = 24
+
+# name = 'Illia '
+
+# print(name + str(age))
+
+
+
+name = 'Illia'
+age = 24
+generall = 'Hello, my name is {0}. I\'m {1} years old.'.format(name, age)
+
+print(generall)
+
+
+float_result = 1000 / 7
+print(float_result)
+print('The result is: {0:1.1f}'.format(float_result))
+
+
+
+name = 'Illia'
+age = 24
+generall = f'Hello, my name is {name}. I\'m {age} years old.'.format(name, age)
+
+print(generall)
